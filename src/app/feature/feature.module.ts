6bc8fa14a7b359ad3b415b2557f71ab294b90466
Meet Dave <meet.dave@outlook.com>
2018-10-23 13:46:38 +0530

    CSS Changes

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FeatureComponent} from './feature.component';
+import { FeatureComponent } from './feature.component';
 import { FeatureRoutingModule } from './feature-routing.module';
 import { MatCardModule } from '@angular/material/card';
-
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FeatureRoutingModule,
-    MatCardModule
+    MatCardModule,
+    SharedModule
   ],
   declarations: [
     FeatureComponent,
